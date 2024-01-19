b4abdc8a8f5598fa4deb91fc7ecd3ecc3d8a2d3e
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-01-19 22:59:14 +0500

    updated

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -12,7 +11,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
