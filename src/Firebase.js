0134137587425ef22350fe368393a9c1e4009139
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-07-27 17:49:24 +0500

    updated

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,8 +8,13 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
+// console.log('API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
+// console.log('Auth Domain:', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
